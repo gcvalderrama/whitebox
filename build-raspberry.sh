db52f2ee2d9d3d0aa7c99c78804d0d31ebde607e
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-02-19 14:07:23 -0500

    add

--- build-raspberry.sh
+++ build-raspberry.sh
@@ -1,9 +1,9 @@
 pushd raspberry
 
-sudo docker build -t whitebox-rb:latest .
+sudo podman build -t whitebox-rb:latest .
 
-sudo docker podman tag whitebox-rb:latest f4phantom.skylab:5000/whitebox-rb:latest
+sudo podman tag whitebox-rb:latest f4phantom.skylab:5000/whitebox-rb:latest
 
-sudo docker push f4phantom.skylab:5000/whitebox-rb:latest 
+sudo podman push f4phantom.skylab:5000/whitebox-rb:latest  --tls-verify=false
 
 popd
--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-pushd raspberry
+pushd worker
 
-podman build -t whitebox:latest .
+sudo podman build -t whitebox:latest .
 
-podman tag whitebox:latest f4phantom.skylab:5000/whitebox:latest
+sudo podman tag whitebox:latest f4phantom.skylab:5000/whitebox:latest
 
-podman push f4phantom.skylab:5000/whitebox:latest --tls-verify=false
+sudo podman push f4phantom.skylab:5000/whitebox:latest --tls-verify=false
 
 popd
