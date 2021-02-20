929023d20c3227961146344c19834562664cfc46
gcvalderrama@hotmail.com <gcvalderrama@hotmail.com>
2021-02-20 03:15:23 +0000

    add

--- build-raspberry.sh
+++ build-raspberry.sh
@@ -1,6 +1,6 @@
-sudo podman build -t whitebox-rb:latest -f DockerfileRaspberry .
+sudo docker build -t whitebox-rb:latest -f DockerfileRaspberry .
 
-sudo podman tag whitebox-rb:latest f4phantom.skylab:5000/whitebox-rb:latest
+sudo docker tag whitebox-rb:latest f4phantom.skylab:5000/whitebox-rb:latest
 
-sudo podman push f4phantom.skylab:5000/whitebox-rb:latest  --tls-verify=false
+sudo docker push f4phantom.skylab:5000/whitebox-rb:latest  --tls-verify=false
 
